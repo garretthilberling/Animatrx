0f7c2d7fdd680cc6bd6ec5112bf1303b5b546f35
garretthilberling <ghilberling@knights.ucf.edu>
2022-08-08 20:49:00 -0400

    printing data to pages now. Finding other useful routes in the API docs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import KitsuApi from "../utils/animeApi";
 
 const Home = () => {
+    const [apiData, setApiData] = useState<any>({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
     useEffect(() => {
-        KitsuApi.getAnimeByGenre("adventure")
+        KitsuApi.getAnimeByTitle("Attack on Titan", setApiData, setLoading, setError);
     }, []);
-
-  return <div className=""></div>;
+    
+    useEffect(() => {
+        console.log(apiData);
+    }, [apiData]);
+    
+  return (
+  <div className="">
+    {loading ? (<div>Loading...</div>) : (
+        <div>
+          {apiData.map((anime: any, index: number) => (
+            <div key={index}>
+                {anime?.attributes?.canonicalTitle}
+            </div>
+          ))}  
+        </div>
+    )}
+  </div>
+  );
 };
 
 export default Home;
--- client/src/utils/animeApi.tsx
+++ client/src/utils/animeApi.tsx
@@ -3,23 +3,61 @@ class KitsuApi {
   getHeaders: object = {
     method: "GET",
     headers: {
-      Accept: "application/vnd.api+json",
-      "Content-Type": "application/vnd.api+json",
+      "Accept": "application/vnd.api+json",
+      "Content-Type": "application/vnd.api+json"
     },
   };
 
-  getAnimeByGenre(genre: string) {
+  getAnimeByGenre(
+    genre: string,
+    setOutput: React.Dispatch<React.SetStateAction<string>>,
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    setError: React.Dispatch<React.SetStateAction<string>>
+  ) {
     let url: string = this.baseUrl + `anime?filter%5Bcategories%5D=${genre}`;
     fetch(url, this.getHeaders)
-    .then((response) => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw response;
       })
       .then((data) => {
-        // setStudentData(data);
-        console.log(data);
+        setOutput(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  getAnimeByTitle(
+    title: string,
+    setOutput: React.Dispatch<React.SetStateAction<string>>,
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    setError: React.Dispatch<React.SetStateAction<string>>
+  ) {
+    let queryTitle = title.replaceAll(" ", "%20");
+    let url = this.baseUrl + `anime?filter%5Btext%5D=${queryTitle}`;
+    fetch(url, this.getHeaders)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setOutput(data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }
 }
