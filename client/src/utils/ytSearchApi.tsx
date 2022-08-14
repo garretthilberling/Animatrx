4fd7224d876edfe8e9e35f4bc41cfda3a5bec269
garretthilberling <ghilberling@knights.ucf.edu>
2022-08-14 12:40:24 -0400

    fixed positioning for anime image cards. Changed border color. May go a little thinner on the border

--- client/src/utils/ytSearchApi.tsx
+++ client/src/utils/ytSearchApi.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { ytSearch } from "./types";
 class YtApi {
-  baseUrl: string = "https://www.googleapis.com/youtube/v3/rest";
+  baseUrl: string = "https://www.googleapis.com/youtube/v3/";
 
   key = process.env.REACT_APP_YT_API_KEY;
+
   getHeaders(baseUrl: string) {
     const response = {
       baseUrl: baseUrl,
@@ -16,15 +18,35 @@ class YtApi {
     return response;
   }
 
+  ytCreateConnection() {
+    axios.create(this.getHeaders(this.baseUrl))
+  }
+
   ytReq(params: string) {
-    axios.create(this.getHeaders(this.baseUrl)).get("/search", {
+    const search: ytSearch = {
+        baseUrl: this.baseUrl,
+        params: {
+          part: "snippet",
+          maxResults: 5,
+          key: this.key,
+        },
+        withCredentials: true,
+      }
+      
+     axios.create(search).get("/search", {
         params: {
           q: params,
         },
-      }).then(response => console.log(response))
-
-    //   Promise.all([init, response])
-    //   .then(data => console.log(data));
+      }).then((response) => {
+        if(response.data.ok) {
+            response.data.json();
+        }
+        throw response.data;
+      })
+      .then(data => {
+        // setOutput(data);
+        console.log(data);
+      })
 
     // console.log(response);
   }
